7789d9af60739808bc7f1b3b06d9c1e3b6afaaf1
yaksas443 <40358371+yaksas443@users.noreply.github.com>
2018-06-25 20:49:22 +0530

    Create lab-init_lin.sh

---
+++ lab-init_lin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#Changing root password
+
+passwd
+
+#Enabling logging on terminal launch and creating log directory
+
+echo 'test "$(ps -ocommand= -p $PPID | awk '"'"'{print $1}'"'"')" == '"'"'script'"'"' || (script -f $HOME/logs/$(date +"%d-%b-%y_%H-%M-%S").${HOSTNAME:-$(hostname)}.$$.${RANDOM}_shell.log)' >> /etc/bash.bashrc
+mkdir $HOME/logs
+
+#Creating WorkDesk
+
+mkdir $HOME/Documents/WorkDesk
+mkdir $HOME/Documents/WorkDesk/yaksas
+
+#Installing Sublime-Text
+
+arch = $(uname -m)
+
+if [ "$arch" = "x86_64" ]
+then
+    wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb
+    dpkg -i sublime-text_build-3083_amd64.deb
+else
+    wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_i386.deb
+    dpkg -i sublime-text_build-3083_i386.deb
+fi
